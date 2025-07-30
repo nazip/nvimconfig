0e6b5310f1fad63b1901f208db677ce06b94f8c1
Nazip <nazipm@mail.ru>
2025-07-30 17:10:10 +0300

    work version

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -35,20 +35,20 @@ return {
                     ['<C-Space>'] = cmp.mapping.complete(),
                     ['<C-e>'] = cmp.mapping.abort(),
                     ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-      ['<Tab>'] = function(fallback)
-        if cmp.visible() then
-          cmp.select_next_item()
-        else
-          fallback() -- If you use vim-endwise, this fallback will behave the same as vim-endwise.
-        end
-      end,
-      ['<S-Tab>'] = function(fallback)
-        if cmp.visible() then
-          cmp.select_prev_item()
-        else
-          fallback() -- If you use vim-endwise, this fallback will behave the same as vim-endwise.
-        end
-      end
+                    -- ['<Tab>'] = function(fallback)
+                    --   if cmp.visible() then
+                    --     cmp.select_next_item()
+                    --   else
+                    --     fallback() -- If you use vim-endwise, this fallback will behave the same as vim-endwise.
+                    --   end
+                    -- end,
+                    -- ['<S-Tab>'] = function(fallback)
+                    --   if cmp.visible() then
+                    --     cmp.select_prev_item()
+                    --   else
+                    --     fallback() -- If you use vim-endwise, this fallback will behave the same as vim-endwise.
+                    --   end
+                    -- end
                 }),
                 sources = cmp.config.sources({
                     { name = 'nvim_lsp' },
---
+++ lua/plugins/git.lua
@@ -0,0 +1,120 @@
+return {
+    {
+        -- GIT BLAME
+        "FabijanZulj/blame.nvim",
+        opts = {
+            date_format = "%Y.%m.%d",
+        },
+        keys = {
+            {
+                "<leader><leader>gb",
+                function() vim.cmd("BlameToggle") end,
+                desc = "Toggle git blame",
+                mode = "n",
+                noremap = true,
+                silent = true
+            },
+        }
+    },
+    {
+        -- GIT CHANGE INDICATOR
+        "lewis6991/gitsigns.nvim",
+        opts = {
+            on_attach = function(bufnr)
+                local gs = package.loaded.gitsigns
+
+                local function map(mode, l, r, opts)
+                    opts = opts or {}
+                    opts.buffer = bufnr
+                    vim.keymap.set(mode, l, r, opts)
+                end
+
+                map('n', ']c', function()
+                    if vim.wo.diff then return ']c' end
+                    vim.schedule(function()
+                        gs.next_hunk()
+                    end)
+                    return '<Ignore>'
+                end, { desc = "next change hunk", expr = true })
+
+                map('n', '[c', function()
+                    if vim.wo.diff then return '[c' end
+                    vim.schedule(function()
+                        gs.prev_hunk()
+                    end)
+                    return '<Ignore>'
+                end, { desc = "prev change hunk", expr = true })
+
+                -- map('n', '<leader><leader>gb',
+                --   function()
+                --     gs.blame_line { full = true }
+                --   end, { desc = "git blame" })
+
+                -- map('n', '<leader><leader>gb',
+                --   function() gs.blame_line {} end,
+                --   { desc = "git blame short" })
+
+                map('n', '<leader><leader>gd', gs.diffthis,
+                    { desc = "git diff (:q to close)" })
+            end
+        }
+    },
+    {
+        -- GIT HISTORY
+        "sindrets/diffview.nvim",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        keys = {
+            {
+                "<leader><leader>gh",
+                "<Cmd>DiffviewFileHistory<CR>",
+                desc = "show project commit history",
+                mode = "n",
+                noremap = true,
+                silent = true
+            },
+            {
+                "<leader><leader>go",
+                "<Cmd>DiffviewOpen<CR>",
+                desc = "show uncommitted file changes",
+                mode = "n",
+                noremap = true,
+                silent = true
+            },
+            {
+                "<leader><leader>gx",
+                "<Cmd>DiffviewClose<CR>",
+                desc = "close diffview window",
+                mode = "n",
+                noremap = true,
+                silent = true
+            },
+        },
+        config = true
+    },
+    {
+        -- OPEN LINES IN GITHUB
+        "ciehanski/nvim-git-line",
+        opts = {
+            action_key = "<leader><leader>gp",
+            action_key_line = "<leader><leader>gl",
+        },
+        config = true
+    },
+    -- FLOATING COMMIT WINDOW
+    {
+        "lsig/messenger.nvim",
+        opts = {
+            border = "rounded"
+        },
+        keys = {
+            {
+                "<leader><leader>gc",
+                "<Cmd>MessengerShow<CR>",
+                desc = "show commit in floating window",
+                mode = "n",
+                noremap = true,
+                silent = true
+            }
+        },
+    },
+}
