96a11817afecf2182d0a8e4491e60af0c3cc726d
Nazip <nazipm@mail.ru>
2026-06-24 11:05:20 +0300

    yaml format fixed

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -2,21 +2,28 @@ return {
 	"stevearc/conform.nvim",
 	opts = {
 		formatters_by_ft = {
-			yaml = { "yamlfmt", "prettier", stop_after_first = true },
+			yaml = { "yamlfmt" },
 		},
 
-		-- Переопределяем поведение форматировщиков
 		formatters = {
 			yamlfmt = {
-				-- Передаем флаг для сохранения пустых строк (line breaks)
-				args = { "-formatter", "retain_line_breaks=true", "-" },
+				-- Переносим YAML-конфигурацию прямо в Lua-аргументы
+				args = function()
+					return {
+						"-formatter",
+						"retain_line_breaks=true,scan_folded_as_literal=true,trim_trailing_whitespace=true",
+						"-",
+					}
+				end,
 			},
 		},
 
-		-- Настройка форматирования при сохранении файла
-		format_on_save = {
-			timeout_ms = 500,
-			lsp_format = "fallback",
-		},
+		-- Полностью изолируем сохранение YAML от вмешательства LSP
+		format_on_save = function(bufnr)
+			return {
+				timeout_ms = 500,
+				lsp_format = "never",
+			}
+		end,
 	},
 }
